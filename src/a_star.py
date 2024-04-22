dd074aca84c29e4c216bdd2c7ab119955e8a7136
Jerry <jeli04.2020@gmail.com>
2024-04-21 23:10:32 -0700

    skeleton code of A* class

--- src/a_star.py
+++ src/a_star.py
@@ -1,3 +1,21 @@
 class a_star:
     def __init__(self) -> None:
         pass
+
+    def call_misplaced_tile(game_board):
+        # calls call_a_starwith the misplaced_tile_heuristic
+        pass
+
+    def call_euclidean_distance(game_board):
+        # calls call_a_starwith the euclidean_distance_heuristic
+        pass
+
+    def call_a_star(game_board, heuristic):
+        pass
+
+    def _misplaced_tile_heuristic(game_board):
+        pass
+
+    def _euclidean_distance_heuristic(game_board):
+        pass
+
