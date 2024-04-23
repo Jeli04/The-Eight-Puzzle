7982e4b3a617362cac65c86ea9f782a0e3810a25
aditithanekar <aditipthanekar@gmail.com>
2024-04-22 23:12:22 -0700

    created runner file to make matrix

--- src/n_puzzle.py
+++ src/n_puzzle.py
@@ -1,5 +1,16 @@
 import numpy as np
 
 class n_puzzle:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, dim, initState, goalState) -> None:
+        self.dim = dim
+        self.initState = initState #make these matrices later 
+        self.goalState = goalState
+    
+class Node:
+    def __init__(self, num, cost, heuristic):
+        self.num = num  # Number of the tile
+        self.cost = cost
+        self.heuristic = heuristic
+        self.parent = parent
+         
+       
