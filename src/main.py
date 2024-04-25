7852249ce407f9c42ad4950f6a574b263f93732a
aditithanekar <aditipthanekar@gmail.com>
2024-04-24 22:45:31 -0700

    called Node class in runner tested printing

--- src/main.py
+++ src/main.py
@@ -1,19 +1,32 @@
+import numpy as np
+from n_puzzle import Node
+from n_puzzle import puzzleProblem
 def main():
     print("Welcome to N-Puzzle Solver")
     puzzleChoice = int(input("Type “1” to use a default puzzle, or “2” to enter your own puzzle."))
+    if(puzzleChoice==1):
+        initial = Node()
+        initial.printPuzzle()
     if(puzzleChoice == 2):
         dim = int(input("Enter the dimension of your n x n puzzle. Ex. (3 x 3) puzzle, enter 3"))
         print("Enter your puzzle, use a zero to represent the blank")
         print("Enter your rows, use space or tabs between numbers 1 2 3")
         
+        # matrix = np.array([])
         matrix = []
 
         for i in range(0, dim):
-            row = []
+            row =[]
+            # row = np.array([])
             
             for j in range(0, dim):
                 row.append(int(input("Enter a value: ")))
             matrix.append(row)
+
+        #matrix = np.asmatrix(matrix)
+        initial = Node(dim, n_puzzle=matrix)
+        initial.printPuzzle()
+        
 
     print("1 for Uniform Cost Search")
     print("2 for A* with the Misplaced Tile heuristic.")
