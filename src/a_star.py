b539eecafe0a62f5c56cc08e899ee6a9d6ee461b
rgujr001 <rgujr001@ucr.edu>
2024-04-28 14:50:57 -0700

    updating euclidean distance heuristic

--- src/a_star.py
+++ src/a_star.py
@@ -17,5 +17,23 @@ class a_star:
         pass
 
     def _euclidean_distance_heuristic(game_board):
-        pass
+        cost = 0
+        curr_row = 0
+        curr_column = 0
+        dimensionality = len(game_board.get_board()[0])
+        correct_val = 1 #meant to indicate what the correct tile at that location in the grid is
+        for row in game_board.get_board():
+            for tile in row:
+                if(correct_val != game_board[row][tile]):
+                    goal_row = correct_val / dimensionality
+                    goal_column = (correct_val % dimensionality) - 1
+
+                    euclidean_distance = math.sqrt(((goal_column - curr_column) ** 2) + ((goal_row - curr_row) ** 2))
+                    cost += euclidean_distance
+                    curr_column += 1
+
+                correct_val += 1
+                curr_row += 1
+                curr_column = 0
+        return cost
 
