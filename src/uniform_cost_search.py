475b15c5653719423c8d61e85e10987b4cfdad35
aditithanekar <aditipthanekar@gmail.com>
2024-05-03 23:22:24 -0700

    fix uniform

--- src/uniform_cost_search.py
+++ src/uniform_cost_search.py
@@ -6,47 +6,41 @@ from n_puzzle import puzzleProblem
 class uniform_cost_search:
     def execute_ucs(self, game):
         
-        heapq.heappush(game.frontier, (0, game))
+        heapq.heappush(game.frontier, (0, game.root))
         
         while game.frontier:
             pair = heapq.heappop(game.frontier)
-            # print("hellooooo")
-            # print(pair[0])
-            # print(pair[1].root.printPuzzle())
-            cur_cost = pair[0]
-            puzzle = pair[1] # we actually want current!
+            node = pair[1] # we actually want current!
+            cur_cost = node.cost
             
             # return current state if goal is found
-            if puzzle.isGoal():
-                print("fgfv")
-                return puzzle
-            else:
-                print("im not the goal!!!!!")
+            if game.isGoal():
+                return game.seen
        
             # mark current node as visited
-            game.seen.add(pair)
+            game.seen.append(node)
             
             # obtain child node from all 4 possible operators
-            for action in puzzle.expandNode(puzzle.root):
+            for action in game.expandNode(node):
                 child = None
-                current = puzzle.root
                 
                 if action == "right":
                     print("right")
-                    child = puzzle.operator_go_right(current)
+                    child = game.operator_go_right(node)
                 elif action == "left":
-                    child = puzzle.operator_go_left(current)
+                    child = game.operator_go_left(node)
                     print("left")
                 elif action == "up":
-                    child = puzzle.operator_go_top(current)
+                    child = game.operator_go_up(node)
                     print("up")
                 elif action == "down":
-                    child = puzzle.operator_go_bottom(current)
+                    child = game.operator_go_down(node)
                     print("down")
             
                 # calculate the child node cost (delete later)
                 # child_cost = child.parent.cost + 1 //already in n_puzzle
                 
+                child_cost = child.parent.cost
                 visited = False
                 
                 #get child cost (child.cost) to use below
@@ -54,26 +48,27 @@ class uniform_cost_search:
                 
                 # update child node if smaller cost is found
                 for pair in game.frontier:
-                    if pair[1].get_board() == child.get_board():
+                    if (pair[1].n_puzzle == child.n_puzzle).all():
                         visited = True
                         
                         if child_cost < cur_cost:
                             pair[1].cost = child_cost
                             pair[1].parent = node
                 
-                for item in game.seen:
-                    if item.get_board() == child.get_board():
-                        visited = True
-                        
-                        if child_cost < cur_cost:
-                            item.cost = child_cost
-                            item.parent = node
-                            game.frontier.heappush((child_cost, item))
+                if visited == False:
+                    for item in game.seen:
+                        if (item.n_puzzle == child.n_puzzle).all():
+                            visited = True
+                            
+                            if child_cost < cur_cost:
+                                item.cost = child_cost
+                                item.parent = node
+                                heapq.heappush(game.frontier, (child_cost, item))
                 
                 # add into frontier if never seen yet
                 if visited == False:
                     child.cost = child_cost
-                    game.frontier.heappush((child_cost, child))
+                    heapq.heappush(game.frontier, (child_cost, item))
                 
             game.seen.append(node)
         
