098c05d172fd19b2b5ecb602f8cd5fb7920f2f5c
Arkes123 <jonathan.darius2015@gmail.com>
2024-05-04 20:53:01 -0700

    report

--- src/report.py
+++ src/report.py
@@ -1,5 +1,7 @@
 import pandas as pd
-import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 # Create dataframe
 
@@ -8,6 +10,7 @@ expansions_df = pd.DataFrame()
 columns = ['Trivial', 'Very Easy', 'Easy', 'Doable', 'Oh Boy']
 
 expansions_df['Test Case'] = columns
-expansions_df['Number of Expansions (A* Misplaced Tile)'] = [0, 1, 2, 4, 5000]
+expansions_df['Number of Expansions (A* Misplaced Tile)'] = [0, 1, 2, 4, 100]
 
-print(expansions_df)
+sns.lineplot(data=expansions_df,x='Test Case', y='Number of Expansions (A* Misplaced Tile)')
+plt.show()
